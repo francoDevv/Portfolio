82a1bd22c374060981535993f987be041d48df3e
francoDevv <votta.franco16@gmail.com>
2023-06-06 20:39:28 -0300

    last details

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 
+let menuIcon = document.querySelector('#menu-icon');
+let nabvar = document.querySelector('.navbar');
 
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    nabvar.classList.toggle('active');
+}
 
 // scroll sections
 let section = document.querySelectorAll('section');
@@ -18,11 +24,23 @@ window.onscroll = () => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
-        };
+            // active sections for animation on scroll
+            sec.classList.add('show-aniamte');
+        }
+        // if want to use animation that repeats on scroll use this
+        else {
+            sec.classList.remove('show-aniamte');
+        }
     });
 
     // sticky header
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    // remove toggle icon and navbar when click navbar links (scroll)
+
+    menuIcon.classList.remove('bx-x');
+    nabvar.classList.remove('active');
+
 }
